8da52db9708b7e2f7338897bae2380fb33ae9053
owocado <24418520+owocado@users.noreply.github.com>
2026-07-16 08:07:01 -0400

    real?
    Removed unused i18n import and updated button label.

--- plugins/delete-embeds/src/index.tsx
+++ plugins/delete-embeds/src/index.tsx
@@ -1,5 +1,5 @@
 import { findByProps } from "@vendetta/metro";
-import { i18n, constants } from "@vendetta/metro/common";
+import { constants } from "@vendetta/metro/common";
 import { after, before} from "@vendetta/patcher";
 import { React } from "@vendetta/metro/common";
 import { getAssetIDByName as getAssetId } from "@vendetta/ui/assets"
@@ -28,11 +28,9 @@ function onLoad() {
                 const canManageMessages = Permissions.can(constants.Permissions.MANAGE_MESSAGES, channel);
                 if (message.embeds.length === 0 || (getCurrentUser().id !== message.author.id && !canManageMessages)) return;
 
-                const label = i18n?.Messages?.WEBHOOK_DELETE_TITLE?.intlMessage?.format({name:"Embed"})
-
                 buttons.push(
                 <ActionSheetRow
-                    label={label || "Suppress Embed"}
+                    label="Suppress Embeds"
                     icon={<ActionSheetRow.Icon source={getAssetId("ic_close_16px")} />}
                     onPress={() => {
                         suppressEmbeds(message.channel_id, message.id)
